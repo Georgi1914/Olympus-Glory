175f091a6940661e57b41c019ca0ecdcbb5c0c23
dashkata <alexandur04@abv.bg>
2022-03-11 19:14:03 +0200

    Adding a SpacePorts file

--- rocket_booker/lib/destinations/TemplateCard.dart
+++ rocket_booker/lib/destinations/TemplateCard.dart
@@ -1,37 +1,32 @@
 import 'package:flutter/material.dart';
 
-class TemplateCard{
+class TemplateCard {
   late String destination;
   late String travelTime;
 
   TemplateCard({required this.destination, required this.travelTime});
 
-  Card createTemplateCard(){
+  Card createTemplateCard() {
     return Card(
-      elevation: 10,
+      elevation: 5,
       child: Column(
         mainAxisSize: MainAxisSize.min,
         children: <Widget>[
           ListTile(
-            leading:CircleAvatar(
+            leading: CircleAvatar(
               backgroundColor: Colors.amber,
             ),
-            title: Text(
-                destination
-            ),
-            subtitle: Text('Travel time: $travelTime'),
+            title: Text(destination),
+            subtitle: Text('$travelTime'),
           ),
           Row(
             mainAxisAlignment: MainAxisAlignment.end,
             children: <Widget>[
               ElevatedButton(
-                  onPressed: (){},
+                  onPressed: () {},
                   child: const Text(
                     'Choose location',
-                    style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 15
-                    ),
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 15),
                   )),
               const SizedBox(width: 8),
             ],
