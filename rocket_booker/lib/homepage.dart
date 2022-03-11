bcb1787c190087524a9fe676434039db1efbf48f
kaloyanchkrv <melhuriank@yahoo.com>
2022-03-11 18:01:24 +0200

    added FAQ button

--- rocket_booker/lib/homepage.dart
+++ rocket_booker/lib/homepage.dart
@@ -1,7 +1,11 @@
+import 'dart:ui';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:rocket_booker/services/authentication.dart';
 import 'package:rocket_booker/userbased/ProfilePage.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'info_page.dart';
 
 class HomePage extends StatefulWidget {
   final User user;
@@ -19,19 +23,23 @@ class _HomePageState extends State<HomePage> {
       appBar: AppBar(
         title: const Text('Home'),
       ),
-      body: Center(
-        child: ElevatedButton(
-          child: const Text('Profile'),
-        onPressed: () {
-        Navigator.push(
-        context,
-        MaterialPageRoute(builder: (context) =>  ProfilePage(user: Authentication.getCurrentUser(),)),
-    );
-    }
-            // Navigate to second route when tapped.
-          ,
-        ),
-      ),
+      body: Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              ElevatedButton(
+                child: const Text('Profile'),
+                onPressed: () {
+                    Navigator.push(context, MaterialPageRoute(builder: (context) =>  ProfilePage(user: Authentication.getCurrentUser(),)),);
+                  },
+              ),
+              ElevatedButton(
+                child: const Text('Information'),
+                onPressed: () {
+                  Navigator.push(context, MaterialPageRoute(builder: (context) => InfoPage()));
+                },
+              ),
+            ],
+          ),
     );
   }
 }
