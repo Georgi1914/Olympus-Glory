60d8eb375a228c15a9f578614220853171bda4c1
Vladi Pavlov <vladi.pavlov26@gmail.com>
2022-03-11 18:53:35 +0200

    added an app bar

--- rocket_booker/lib/destinations/destinationsPage.dart
+++ rocket_booker/lib/destinations/destinationsPage.dart
@@ -9,6 +9,7 @@ class destinationsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(),
       body: SafeArea(
         child: Column(
           children: [
