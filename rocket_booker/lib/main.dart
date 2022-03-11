7be3ce272b790ce0024df5aae820d82be106508f
dashkata <alexandur04@abv.bg>
2022-03-11 19:21:29 +0200

    Added the main file after deleting it

---
+++ rocket_booker/lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:rocket_booker/SpacePort/SpacePorts.dart';
+import 'package:rocket_booker/homepage.dart';
+import 'package:rocket_booker/info_page.dart';
+import 'package:rocket_booker/userbased/LoginPage.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:rocket_booker/reviews/reviews_page.dart';
+import 'package:rocket_booker/services/authentication.dart';
+import 'package:rocket_booker/userbased/LoginPage.dart';
+import 'package:rocket_booker/userbased/ProfilePage.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  // await Firebase.initializeApp(
+  //   options: DefaultFirebaseOptions.currentPlatform,
+  // );
+  FirebaseApp defaultApp = await Firebase.initializeApp();
+  runApp(MaterialApp(
+      initialRoute: '/login',
+      routes: {
+        '/login': (context) => loginPage(),
+        '/home': (context) => HomePage(user: Authentication.getCurrentUser()),
+        '/profile': (context) =>
+            ProfilePage(user: Authentication.getCurrentUser()),
+        '/FAQ': (context) => InfoPage(),
+        '/SpacePorts': ((context) => SpacePorts()),
+        '/SpacePortsDates': ((context) => SpacePorts()),
+      },
+      home: loginPage()));
+}
