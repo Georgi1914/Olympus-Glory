03569f8bdaf2054310d2e729d3c581a24ec2d734
bobomaika <georgistan@abv.bg>
2022-03-11 18:21:03 +0200

    minor changes to info_page.dart

--- rocket_booker/lib/info_page.dart
+++ rocket_booker/lib/info_page.dart
@@ -9,7 +9,17 @@ class InfoPage extends StatelessWidget {
       appBar: AppBar(
         title: Text('FAQ & Info Page'),
       ),
-      body: Text('dokimenti, bagaji, ebalo si e mami'),
+      body: Container(
+        padding: EdgeInsets.fromLTRB(10.0, 10.0, 10.0, 0.0),
+        child: Text(
+          'Some of the things you might consider taking with you on a trip in Space are:\n - Personal luggage(includes anything from a toothbrush and clothes to medicine)\n',
+          // textAlign: TextAlign.center,
+          // overflow: TextOverflow.ellipsis,
+          style: TextStyle(
+            fontSize: 15.0,
+          ),
+        ),
+      ),
     );
   }
 }
