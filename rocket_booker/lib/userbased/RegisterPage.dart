11b3bbd23e8ef3735ac349ee4da36d860c6f1df1
Georgi1914 <gyanakiev11@gmail.com>
2022-03-12 01:50:11 +0200

    Login and register page, login button on the register page + routing

--- rocket_booker/lib/userbased/RegisterPage.dart
+++ rocket_booker/lib/userbased/RegisterPage.dart
@@ -1,7 +1,9 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:rocket_booker/services/authentication.dart';
+import 'LoginPage.dart';
 import 'ProfilePage.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'validation.dart';
@@ -34,114 +36,163 @@ class _RegisterPageState extends State<RegisterPage> {
         _focusPassword.unfocus();
       },
       child: Scaffold(
-        appBar: AppBar(
-          title: Text('Register'),
-        ),
-        body: Padding(
-          padding: const EdgeInsets.all(24.0),
-          child: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Form(
-                  key: _registerFormKey,
-                  child: Column(
-                    children: <Widget>[
-                      TextFormField(
-                        controller: _nameTextController,
-                        focusNode: _focusName,
-                        validator: (value) => Validator.validateName(
-                          name: value as String,
-                        ),
-                        decoration: InputDecoration(
-                          hintText: "Name",
-                          errorBorder: UnderlineInputBorder(
-                            borderRadius: BorderRadius.circular(6.0),
-                            borderSide: BorderSide(
-                              color: Colors.red,
+        resizeToAvoidBottomInset: false,
+        body: Container(
+          width: double.infinity,
+          decoration: const BoxDecoration(
+            image: DecorationImage(
+                image: AssetImage("assets/Space.jpg"),
+                fit: BoxFit.cover),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.all(24.0),
+            child: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Form(
+                    key: _registerFormKey,
+                    child: Column(
+                      children: <Widget>[
+                        TextFormField(
+                          style: TextStyle(fontSize: 20, color: Colors.deepPurple[800], fontWeight: FontWeight.bold),
+                          controller: _nameTextController,
+                          focusNode: _focusName,
+                          validator: (value) => Validator.validateName(
+                            name: value as String,
+                          ),
+                          decoration: InputDecoration(
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(10.0),
                             ),
+                            filled: true,
+                            hintStyle: TextStyle(color: Colors.deepPurple[800]?.withAlpha(100), fontSize: 20, fontWeight: FontWeight.bold),
+                            hintText: "Name",
+                            fillColor: Colors.white,
                           ),
                         ),
-                      ),
-                      SizedBox(height: 16.0),
-                      TextFormField(
-                        controller: _emailTextController,
-                        focusNode: _focusEmail,
-                        validator: (value) => Validator.validateEmail(
-                          email: value as String,
-                        ),
-                        decoration: InputDecoration(
-                          hintText: "Email",
-                          errorBorder: UnderlineInputBorder(
-                            borderRadius: BorderRadius.circular(6.0),
-                            borderSide: BorderSide(
-                              color: Colors.red,
-                            ),
+                        SizedBox(height: 16.0),
+                        TextFormField(
+                          style: TextStyle(fontSize: 20, color: Colors.deepPurple[800], fontWeight: FontWeight.bold),
+                          controller: _emailTextController,
+                          focusNode: _focusEmail,
+                          validator: (value) => Validator.validateEmail(
+                            email: value as String,
                           ),
-                        ),
-                      ),
-                      SizedBox(height: 16.0),
-                      TextFormField(
-                        controller: _passwordTextController,
-                        focusNode: _focusPassword,
-                        obscureText: true,
-                        validator: (value) => Validator.validatePassword(
-                          password: value as String,
-                        ),
-                        decoration: InputDecoration(
-                          hintText: "Password",
-                          errorBorder: UnderlineInputBorder(
-                            borderRadius: BorderRadius.circular(6.0),
-                            borderSide: BorderSide(
-                              color: Colors.red,
+                          decoration: InputDecoration(
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(10.0),
                             ),
-                          ),
-                        ),
-                      ),
-                      SizedBox(height: 32.0),
-                      _isProcessing
-                          ? CircularProgressIndicator()
-                          : Row(
-                              children: [
-                                Expanded(
-                                  child: ElevatedButton(
-                                    onPressed: () async {
-                                      setState(() {
-                                        _isProcessing = true;
-                                      });
 
-                                      if (_registerFormKey.currentState!
-                                          .validate()) {
-                                        User? user = await Authentication
-                                            .registerUsingEmailPassword(
-                                          name: _nameTextController.text,
-                                          email: _emailTextController.text,
-                                          password:
-                                              _passwordTextController.text,
-                                        );
+                            filled: true,
+                            hintStyle: TextStyle(color: Colors.deepPurple[800]?.withAlpha(100), fontSize: 20, fontWeight: FontWeight.bold),
+                            hintText: "Email",
+                            fillColor: Colors.white,
+                          ),
+                        ),
+                        SizedBox(height: 16.0),
+                        TextFormField(
+                          style: TextStyle(fontSize: 20, color: Colors.deepPurple[800], fontWeight: FontWeight.bold),
+                          controller: _passwordTextController,
+                          focusNode: _focusPassword,
+                          obscureText: true,
+                          validator: (value) => Validator.validatePassword(
+                            password: value as String,
+                          ),
+                          decoration: InputDecoration(
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(10.0),
+                            ),
+                            filled: true,
+                            hintStyle: TextStyle(color: Colors.deepPurple[800]?.withAlpha(100), fontSize: 20, fontWeight: FontWeight.bold),
+                            hintText: "Password",
+                            fillColor: Colors.white,
+                          ),
+                        ),
+                        SizedBox(height: 20.0),
+                        _isProcessing
+                            ? CircularProgressIndicator()
+                            : Column(
+                                children: [
+                                  Padding(
+                                    padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
+                                    child: Expanded(
+                                      child: SizedBox(
+                                        width: 230,
+                                        height: 50,
+                                        child: ElevatedButton(
+                                          style: ElevatedButton.styleFrom(primary: Colors.deepPurple[800]),
+                                          onPressed: () async {
+                                            setState(() {
+                                              _isProcessing = true;
+                                            });
 
-                                        setState(() {
-                                          _isProcessing = false;
-                                          _registrationMessage =
-                                              "Sucessfully registered";
-                                        });
-                                      } else {
-                                        _isProcessing = false;
-                                      }
-                                    },
-                                    child: Text(
-                                      'Sign up',
-                                      style: TextStyle(color: Colors.white),
+                                            if (_registerFormKey.currentState!
+                                                .validate()) {
+                                              User? user = await Authentication
+                                                  .registerUsingEmailPassword(
+                                                name: _nameTextController.text,
+                                                email: _emailTextController.text,
+                                                password:
+                                                    _passwordTextController.text,
+                                              );
+
+                                              setState(() {
+                                                _isProcessing = false;
+                                                _registrationMessage =
+                                                    "Sucessfully registered";
+                                              });
+                                            } else {
+                                              _isProcessing = false;
+                                            }
+                                          },
+                                          child: Text(
+                                            'Start your journey',
+                                            style: TextStyle(color: Colors.white, fontSize: 23),
+                                          ),
+                                        ),
+                                      ),
                                     ),
                                   ),
-                                ),
-                              ],
-                            ),
-                      Text(_registrationMessage),
-                    ],
-                  ),
-                )
-              ],
+                                  SizedBox(height: 150.0),
+                                  Padding(
+                                    padding: const EdgeInsets.fromLTRB(0, 0, 0, 0),
+                                    child: Text(
+                                      'Or if you already have\n an account',
+                                      textAlign: TextAlign.center,
+                                      style: TextStyle(color: Colors.white, fontSize: 20, fontWeight: FontWeight.bold)
+                                    ),
+                                  ),
+                                  Padding(
+                                    padding: const EdgeInsets.fromLTRB(0, 5, 0, 0),
+                                    child: Expanded(
+                                        child: SizedBox(
+                                          width: 110,
+                                          height: 50,
+                                          child: ElevatedButton(
+                                            style: ElevatedButton.styleFrom(primary: Colors.deepPurple[800]),
+                                            onPressed: () {
+                                              Navigator.of(context).pushReplacement(
+                                                MaterialPageRoute(
+                                                    builder: (context) => loginPage()),
+                                              );
+                                            },
+                                            child: Text(
+                                                'Sign In',
+                                                style: TextStyle(color: Colors.white, fontSize: 20),
+                                          ),
+                                          ),
+                                        ),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                        Text(_registrationMessage),
+                      ],
+                    ),
+                  )
+                ],
+              ),
             ),
           ),
         ),
