ebfb95d79c645b197865c867b795e6849b7bc6e0
dashkata <alexandur04@abv.bg>
2022-03-11 12:34:44 +0200

    Added the sign in and sign up page and merging the project with firebase auth

---
+++ rocket_booker/lib/userbased/LoginPage.dart
@@ -0,0 +1,96 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'validation.dart';
+import 'package:rocket_booker/services/authentication.dart';
+import 'RegisterPage.dart';
+import 'ProfilePage.dart';
+
+class loginPage extends StatefulWidget {
+  const loginPage({Key? key}) : super(key: key);
+
+  @override
+  State<loginPage> createState() => _loginPageState();
+}
+
+class _loginPageState extends State<loginPage> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  final TextEditingController _emailTextController = TextEditingController();
+  final TextEditingController _passwordTextController = TextEditingController();
+
+  // FocusNode _focusEmail = FocusNode();
+  // FocusNode _focusPassword = FocusNode();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("Login Page"),
+        ),
+        body: Form(
+          key: _formKey,
+          child: Column(
+            children: [
+              TextFormField(
+                controller: _emailTextController,
+                // focusNode: _focusEmail,
+                validator: (value) =>
+                    Validator.validateEmail(email: value as String),
+              ),
+              SizedBox(height: 8.0),
+              TextFormField(
+                controller: _passwordTextController,
+                // focusNode: _focusPassword,
+                obscureText: true,
+                validator: (value) =>
+                    Validator.validatePassword(password: value as String),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Expanded(
+                    child: ElevatedButton(
+                      onPressed: () async {
+                        if (_formKey.currentState!.validate()) {
+                          User? user =
+                              await Authentication.signInUsingEmailPassword(
+                            email: _emailTextController.text,
+                            password: _passwordTextController.text,
+                            context: context,
+                          );
+                          if (user != null) {
+                            Navigator.of(context).pushReplacement(
+                              MaterialPageRoute(
+                                  builder: (context) =>
+                                      ProfilePage(user: user)),
+                            );
+                          }
+                        }
+                      },
+                      child: Text(
+                        'Sign In',
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: ElevatedButton(
+                      onPressed: () {
+                        Navigator.of(context).push(
+                          MaterialPageRoute(
+                              builder: (context) => RegisterPage()),
+                        );
+                      },
+                      child: Text(
+                        'Register',
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ),
+                  ),
+                ],
+              )
+            ],
+          ),
+        ));
+  }
+}
