c34b618d1b05d31ada316cbd9927e30968d983f4
dashkata <alexandur04@abv.bg>
2022-03-12 15:17:35 +0200

    Connected the confirmation page to the SpacePorts page

--- rocket_booker/lib/main.dart
+++ rocket_booker/lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:rocket_booker/SpacePort/SpacePorts.dart';
-import 'package:rocket_booker/SpacePort/SpacePortsDates.dart';
 import 'package:rocket_booker/SpacePort/confirmation.dart';
 import 'package:rocket_booker/destinations/destinationsPage.dart';
 import 'package:rocket_booker/homepage.dart';
@@ -21,7 +20,7 @@ void main() async {
   // );
   FirebaseApp defaultApp = await Firebase.initializeApp();
   runApp(MaterialApp(
-      initialRoute: '/confirmation',
+      initialRoute: '/login',
       routes: {
         '/login': (context) => loginPage(),
         '/register': (context) => RegisterPage(),
@@ -32,7 +31,9 @@ void main() async {
         '/SpacePorts': ((context) => SpacePorts()),
         '/reviews': ((context) => ReviewsPage()),
         '/destinations': ((context) => destinationsPage()),
-        '/confirmation': ((context) => confirmationPage())
+        '/confirmation': ((context) => confirmationPage(
+              user: Authentication.getCurrentUser(),
+            ))
       },
       home: loginPage()));
 }
