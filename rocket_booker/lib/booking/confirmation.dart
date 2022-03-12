70d5b4581aa0b789fe55c2698f6fbc8bd8a33f17
dashkata <alexandur04@abv.bg>
2022-03-13 00:15:17 +0200

    Added some of the file functionations

--- rocket_booker/lib/booking/confirmation.dart
+++ rocket_booker/lib/booking/confirmation.dart
@@ -4,6 +4,8 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:rocket_booker/services/authentication.dart';
+import 'package:file_picker/file_picker.dart';
+import 'package:firebase_storage/firebase_storage.dart' as firebase_storage;
 
 class confirmationPage extends StatelessWidget {
   final User user;
@@ -13,143 +15,159 @@ class confirmationPage extends StatelessWidget {
 
   late User _currentUser = user;
 
-  void addData(Map data){
+  void addData(Map data) {
     final userUid = _currentUser.uid;
-    users.doc(userUid).set(data,SetOptions(merge: true),);
+    users.doc(userUid).set(
+          data,
+          SetOptions(merge: true),
+        );
   }
+  /*
+  Future<void> uploadFile() async {
+    try {
+      await firebase_storage.FirebaseStorage.instance
+          .ref('uploads/file-to-upload.png')
+          .putFile();
+    } on FirebaseException catch (e) {
+      // e.g, e.code == 'canceled'
+    }
+  }*/
 
   Widget build(BuildContext context) {
     final selectedDate =
-    ModalRoute
-        .of(context)!
-        .settings
-        .arguments as Map<dynamic, dynamic>;
+        ModalRoute.of(context)!.settings.arguments as Map<dynamic, dynamic>;
     return Scaffold(
         body: Container(
-          width: double.infinity,
-          decoration: const BoxDecoration(
-            image: DecorationImage(
-                image: AssetImage("assets/Space.jpg"), fit: BoxFit.cover),
-          ),
-          child: Column(
-            children: [
-              Padding(
-                padding: const EdgeInsets.fromLTRB(30, 110, 30, 0),
-                child: Container(
-                  decoration: BoxDecoration(
-                    color: Colors.white70,
-                    border: Border.all(color: Colors.black, width: 3),
+      width: double.infinity,
+      decoration: const BoxDecoration(
+        image: DecorationImage(
+            image: AssetImage("assets/Space.jpg"), fit: BoxFit.cover),
+      ),
+      child: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.fromLTRB(30, 110, 30, 0),
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.white70,
+                border: Border.all(color: Colors.black, width: 3),
+              ),
+              child: Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(0, 20, 0, 0),
+                    child: Text(
+                      'NAME',
+                      style: TextStyle(
+                        color: Colors.grey[900],
+                        letterSpacing: 2.0,
+                        fontSize: 15.0,
+                      ),
+                    ),
                   ),
-                  child: Column(
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.fromLTRB(0, 20, 0, 0),
-                        child: Text(
-                          'NAME',
+                  SizedBox(height: 10.0),
+                  Text(
+                    "${_currentUser.displayName}",
+                    // ${_currentUser.displayName}'
+                    style: TextStyle(
+                      color: Colors.grey[900],
+                      //fontWeight: FontWeight.bold,
+                      fontSize: 28.0,
+                      letterSpacing: 2.0,
+                    ),
+                  ),
+                  Divider(
+                    height: 60,
+                    thickness: 3.0,
+                    color: Colors.grey[800],
+                  ),
+                  for (var entries in selectedDate.entries)
+                    Column(
+                      children: [
+                        Text(
+                          entries.key,
                           style: TextStyle(
                             color: Colors.grey[900],
                             letterSpacing: 2.0,
                             fontSize: 15.0,
                           ),
                         ),
-                      ),
-                      SizedBox(height: 10.0),
-                      Text(
-                        "${_currentUser.displayName}",
-                        // ${_currentUser.displayName}'
-                        style: TextStyle(
-                          color: Colors.grey[900],
-                          //fontWeight: FontWeight.bold,
-                          fontSize: 28.0,
-                          letterSpacing: 2.0,
-                        ),
-                      ),
-                      Divider(
-                        height: 60,
-                        thickness: 3.0,
-                        color: Colors.grey[800],
-                      ),
-                      for (var entries in selectedDate.entries)
-                        Column(
-                          children: [
-                            Text(
-                              entries.key,
-                              style: TextStyle(
-                                color: Colors.grey[900],
-                                letterSpacing: 2.0,
-                                fontSize: 15.0,
-                              ),
-                            ),
-                            SizedBox(height: 10.0),
-                            Text(
-                              entries.value,
-                              // ${_currentUser.displayName}'
-                              style: TextStyle(
-                                color: Colors.grey[900],
-                                //fontWeight: FontWeight.bold,
-                                fontSize: 28.0,
-                                letterSpacing: 2.0,
-                              ),
-                            ),
-                            Divider(
-                              height: 50,
-                              thickness: 3.0,
-                              color: Colors.grey[800],
-                            ),
-                          ],
-                        ),
-                      Column(
-                        children: [
-                          ElevatedButton.icon(
-                            onPressed: () {},
-                            icon: Icon(
-                              Icons.attachment,
-                              color: Colors.white,
-                              size: 24.0,
-                            ),
-                            style: ElevatedButton.styleFrom(
-                              primary: Colors.deepPurple,
-                            ),
-                            label: Text("Attach file"),
+                        SizedBox(height: 10.0),
+                        Text(
+                          entries.value,
+                          // ${_currentUser.displayName}'
+                          style: TextStyle(
+                            color: Colors.grey[900],
+                            //fontWeight: FontWeight.bold,
+                            fontSize: 28.0,
+                            letterSpacing: 2.0,
                           ),
-                          ElevatedButton(
-                            onPressed: () {
-                              addData(selectedDate);
-                            },
-                            style: ElevatedButton.styleFrom(
-                              primary: Colors.deepPurple,
-                            ),
-                            child: Text("Confirm booking"),
-                          ),
-                        ],
+                        ),
+                        Divider(
+                          height: 50,
+                          thickness: 3.0,
+                          color: Colors.grey[800],
+                        ),
+                      ],
+                    ),
+                  Column(
+                    children: [
+                      ElevatedButton.icon(
+                        onPressed: () async {
+                          final result = await FilePicker.platform.pickFiles();
+
+                          if (result != null) {
+                            // uploadFile(result.files[0]);
+                          }
+                        },
+                        icon: Icon(
+                          Icons.attachment,
+                          color: Colors.white,
+                          size: 24.0,
+                        ),
+                        style: ElevatedButton.styleFrom(
+                          primary: Colors.deepPurple,
+                        ),
+                        label: Text("Attach file"),
+                      ),
+                      ElevatedButton(
+                        onPressed: () {
+                          addData(selectedDate);
+                        },
+                        style: ElevatedButton.styleFrom(
+                          primary: Colors.deepPurple,
+                        ),
+                        child: Text("Confirm booking"),
                       ),
                     ],
                   ),
-                ),
+                ],
               ),
-              Spacer(),
-              Align(
-                alignment: Alignment.bottomLeft,
-                child:Padding(
-                  padding: const EdgeInsets.fromLTRB(15, 0, 0, 15),
-                  child: ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                        primary: Colors.deepPurple[600],
-                        side: BorderSide(width: 3, color: Colors.deepPurple),
-                        shape: const CircleBorder(),
-                        padding: const EdgeInsets.all(18)),
-                    child: const Icon(
-                      Icons.arrow_back_rounded,
-                      size: 30,
-                    ),
-                    onPressed: () {
-                      Navigator.pop(context);
-                    },
-                  ),
-                ),
-              ),
-            ],
+            ),
           ),
-        ));
+          Spacer(),
+          Align(
+            alignment: Alignment.bottomLeft,
+            child: Padding(
+              padding: const EdgeInsets.fromLTRB(15, 0, 0, 15),
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                    primary: Colors.deepPurple[600],
+                    side: BorderSide(width: 3, color: Colors.deepPurple),
+                    shape: const CircleBorder(),
+                    padding: const EdgeInsets.all(18)),
+                child: const Icon(
+                  Icons.arrow_back_rounded,
+                  size: 30,
+                ),
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+              ),
+            ),
+          ),
+        ],
+      ),
+    ));
   }
 }
