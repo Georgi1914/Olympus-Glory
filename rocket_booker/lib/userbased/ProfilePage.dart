4898c34d39f359e58e403f3c7b00c0aadc9bc322
bobomaika <georgistan@abv.bg>
2022-03-12 04:11:16 +0200

    profile page update

--- rocket_booker/lib/userbased/ProfilePage.dart
+++ rocket_booker/lib/userbased/ProfilePage.dart
@@ -1,6 +1,5 @@
-import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'LoginPage.dart';
+import 'package:flutter/material.dart';
 
 class ProfilePage extends StatefulWidget {
   final User user;
@@ -24,48 +23,120 @@ class _ProfilePageState extends State<ProfilePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Profile'),
+        title: const Text('Your Profile'),
+        centerTitle: true,
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              'NAME: ${_currentUser.displayName}',
-              style: Theme.of(context).textTheme.bodyText1,
+      body: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 70.0, horizontal: 30.0),
+        child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+          const Center(
+            child: CircleAvatar(
+              backgroundColor: Colors.grey,
+              radius: 50.0,
+              child: Image(image: AssetImage('assets/avatar.png')),
             ),
-            SizedBox(height: 16.0),
-            Text(
-              'EMAIL: ${_currentUser.email}',
-              style: Theme.of(context).textTheme.bodyText1,
+          ),
+          Divider(
+            height: 60,
+            thickness: 3.0,
+            color: Colors.grey[800],
+          ),
+          SizedBox(height: 20.0),
+          Text(
+            'NAME',
+            style: TextStyle(
+              color: Colors.grey[900],
+              letterSpacing: 2.0,
+              fontSize: 15.0,
             ),
-            SizedBox(height: 16.0),
-            _currentUser.emailVerified
-                ? Text(
-                    'Email verified',
-                    style: Theme.of(context)
-                        .textTheme
-                        .bodyText1!
-                        .copyWith(color: Colors.green),
-                  )
-                : Text(
-                    'Email not verified',
-                    style: Theme.of(context)
-                        .textTheme
-                        .bodyText1!
-                        .copyWith(color: Colors.red),
-                  ),
-            ElevatedButton(
-                onPressed: () async {
-                  await FirebaseAuth.instance.signOut();
-                  Navigator.pushNamedAndRemoveUntil(context, "/login", (r) => false);
-                },
-                child: Text('Sign out'))
-            // Add widgets for verifying email
-            // and, signing out the user
-          ],
-        ),
+          ),
+          SizedBox(height: 10.0),
+          Text(
+            '${_currentUser.displayName}',
+            style: TextStyle(
+              color: Colors.grey[900],
+              //fontWeight: FontWeight.bold,
+              fontSize: 28.0,
+              letterSpacing: 2.0,
+            ),
+          ),
+          SizedBox(height: 60.0),
+          Row(
+            children: [
+              Icon(Icons.mail_sharp),
+              SizedBox(width: 10.0),
+              Text(
+                '${_currentUser.email}',
+                style: TextStyle(
+                  color: Colors.grey[900],
+                  fontSize: 18.0,
+                  letterSpacing: 1.0,
+                ),
+              ),
+            ],
+          ),
+          SizedBox(height: 30.0),
+          Row(
+            children: [
+              Icon(
+                Icons.verified,
+                color: Colors.green,
+              ),
+              SizedBox(width: 10.0),
+              Text(
+                'Your email is verified',
+                style: TextStyle(
+                  color: Colors.grey[900],
+                  fontSize: 18.0,
+                  letterSpacing: 1.0,
+                ),
+              ),
+            ],
+          ),
+        ]),
       ),
     );
+
+    // body: Center(
+    //   child: Column(
+    //     mainAxisAlignment: MainAxisAlignment.center,
+    //     children: [
+    //       Text(
+    //         'NAME: ${_currentUser.displayName}',
+    //         style: Theme.of(context).textTheme.bodyText1,
+    //       ),
+    //       SizedBox(height: 16.0),
+    //       Text(
+    //         'EMAIL: ${_currentUser.email}',
+    //         style: Theme.of(context).textTheme.bodyText1,
+    //       ),
+    //       SizedBox(height: 16.0),
+    //       _currentUser.emailVerified
+    //           ? Text(
+    //               'Email verified',
+    //               style: Theme.of(context)
+    //                   .textTheme
+    //                   .bodyText1!
+    //                   .copyWith(color: Colors.green),
+    //             )
+    //           : Text(
+    //               'Email not verified',
+    //               style: Theme.of(context)
+    //                   .textTheme
+    //                   .bodyText1!
+    //                   .copyWith(color: Colors.red),
+    //             ),
+    //       ElevatedButton(
+    //           onPressed: () async {
+    //             await FirebaseAuth.instance.signOut();
+    //             Navigator.pushNamedAndRemoveUntil(
+    //                 context, "/login", (r) => false);
+    //           },
+    //           child: Text('Sign out'))
+    //       // Add widgets for verifying email
+    //       // and, signing out the user
+    //     ],
+    //   ),
+    // ),
   }
 }
