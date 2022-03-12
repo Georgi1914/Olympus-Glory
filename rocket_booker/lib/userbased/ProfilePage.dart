75f7c25bc086f215e4f36a87ff9e2e89e7ab49b6
bobomaika <georgistan@abv.bg>
2022-03-12 23:43:02 +0200

    Profile page changing profile image

--- rocket_booker/lib/userbased/ProfilePage.dart
+++ rocket_booker/lib/userbased/ProfilePage.dart
@@ -4,6 +4,7 @@ import 'dart:io';
 
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:image_picker/image_picker.dart';
 
 class ProfilePage extends StatefulWidget {
@@ -14,6 +15,7 @@ class ProfilePage extends StatefulWidget {
 }
 
 class _ProfilePageState extends State<ProfilePage> {
+  File? image;
   PickedFile? imageFile;
   final ImagePicker picker = ImagePicker();
   bool _isSendingVerification = false;
@@ -36,18 +38,11 @@ class _ProfilePageState extends State<ProfilePage> {
       body: Padding(
         padding: const EdgeInsets.symmetric(vertical: 70.0, horizontal: 30.0),
         child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-          // const Center(
-          //   child: CircleAvatar(
-          //     backgroundColor: Colors.grey,
-          //     radius: 50.0,
-          //     child: Image(image: AssetImage('assets/avatar.png')),
-          //   ),
-          // ),
           ProfileImage(),
           Divider(
             height: 60,
-            thickness: 3.0,
-            color: Colors.grey[800],
+            thickness: 2.0,
+            color: Colors.grey[600],
           ),
           SizedBox(height: 20.0),
           Text(
@@ -106,98 +101,156 @@ class _ProfilePageState extends State<ProfilePage> {
     );
   }
 
-  void takePhoto(ImageSource source) async {
-    final pickedFile = await picker.pickImage(source: source);
-
-    setState(() {
-      imageFile = pickedFile as PickedFile;
-    });
-  }
+  // void takePhoto(ImageSource source) async {
+  //   final pickedFile = await picker.pickImage(source: source);
+  //
+  //   setState(() {
+  //     imageFile = pickedFile as PickedFile;
+  //   });
+  // }
 
   Widget ProfileImage() {
-    return Center(
-      child: Stack(
-        children: [
-          CircleAvatar(
-            radius: 60.0,
-            backgroundColor: Colors.grey,
-            backgroundImage: imageFile?.path != ""
-                ? AssetImage('assets/avatar.png')
-                : FileImage(File(imageFile?.path as String)) as ImageProvider,
-          ),
-          Positioned(
-            bottom: 20.0,
-            right: 20.0,
-            child: InkWell(
-              onTap: () {
-                showModalBottomSheet(
-                  context: context,
-                  builder: ((builder) => bottomSheet()),
-                );
-              },
-              child: Icon(
-                Icons.camera_alt,
-                size: 25.0,
-                color: Colors.grey[600],
+    if (this.image == null) {
+      return Center(
+        child: Stack(
+          children: [
+            CircleAvatar(
+              radius: 60.0,
+              backgroundColor: Colors.grey,
+              backgroundImage: AssetImage('assets/avatar.png'),
+              //FileImage(image as File),
+            ),
+            Positioned(
+              bottom: 10.0,
+              right: 10.0,
+              child: InkWell(
+                onTap: () {
+                  // showModalBottomSheet(
+                  //   context: context,
+                  //   builder: ((builder) => showImageSource(context)),
+                  // );
+                  showImageSource(context);
+                },
+                child: Icon(
+                  Icons.camera_alt,
+                  size: 25.0,
+                  color: Colors.amber,
+                ),
               ),
             ),
+          ],
+        ),
+      );
+    } else {
+      return Center(
+        child: Stack(
+          children: [
+            CircleAvatar(
+              radius: 60.0,
+              backgroundColor: Colors.grey,
+              backgroundImage: FileImage(image as File),
+              //FileImage(image as File),
+            ),
+            Positioned(
+              bottom: 10.0,
+              right: 10.0,
+              child: InkWell(
+                onTap: () {
+                  // showModalBottomSheet(
+                  //   context: context,
+                  //   builder: ((builder) => showImageSource(context)),
+                  // );
+                  showImageSource(context);
+                },
+                child: Icon(
+                  Icons.camera_alt,
+                  size: 25.0,
+                  color: Colors.amber,
+                ),
+              ),
+            ),
+          ],
+        ),
+      );
+    }
+  }
+
+  Future<Future> showImageSource(BuildContext context) async {
+    return showModalBottomSheet(
+      context: context,
+      builder: (context) => Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          ListTile(
+            leading: Icon(Icons.camera_alt),
+            title: Text('Camera'),
+            onTap: () => pickImage(ImageSource.camera),
           ),
+          ListTile(
+            leading: Icon(Icons.image),
+            title: Text('Gallery'),
+            onTap: () => pickImage(ImageSource.gallery),
+          )
         ],
       ),
     );
   }
 
-  Widget bottomSheet() {
-    return Container(
-      height: 100.0,
-      width: MediaQuery.of(context).size.width,
-      margin: EdgeInsets.symmetric(horizontal: 20.0, vertical: 20.0),
-      child: Column(
-        children: [
-          Text(
-            'Choose a profile picture',
-            style: TextStyle(
-              fontSize: 20.0,
-            ),
-          ),
-          SizedBox(
-            height: 20.0,
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              // FlatButton.icon(
-              //   onPressed: () {
-              //     takePhoto(ImageSource.camera);
-              //   },
-              //   icon: Icon(Icons.camera_alt),
-              //   label: Text('Camera'),
-              // ),
-              TextButton.icon(
-                onPressed: () {
-                  takePhoto(ImageSource.camera);
-                },
-                icon: Icon(Icons.camera_alt),
-                label: Text('Camera'),
-              ),
-              TextButton.icon(
-                onPressed: () {
-                  takePhoto(ImageSource.gallery);
-                },
-                icon: Icon(Icons.add_photo_alternate),
-                label: Text('Gallery'),
-              ),
-              // FlatButton.icon(
-              //   onPressed: () {
-              //     takePhoto(ImageSource.gallery);
-              //   },
-              //   icon: Icon(Icons.add_photo_alternate),
-              //   label: Text('Gallery'),
-              // ),
-            ],
-          ),
-        ],
-      ),
-    );
+  // Widget bottomSheet() {
+  //   return Container(
+  //     height: 100.0,
+  //     width: MediaQuery.of(context).size.width,
+  //     margin: EdgeInsets.symmetric(horizontal: 20.0, vertical: 20.0),
+  //     child: Column(
+  //       children: [
+  //         Text(
+  //           'Choose a profile picture',
+  //           style: TextStyle(
+  //             fontSize: 20.0,
+  //           ),
+  //         ),
+  //         SizedBox(
+  //           height: 20.0,
+  //         ),
+  //         Row(
+  //           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+  //           children: [
+  //             TextButton.icon(
+  //               onPressed: () {
+  //                 pickImage(ImageSource.camera);
+  //               },
+  //               icon: Icon(Icons.camera_alt),
+  //               label: Text('Camera'),
+  //             ),
+  //             TextButton.icon(
+  //               onPressed: () {
+  //                 pickImage(ImageSource.gallery);
+  //               },
+  //               icon: Icon(Icons.add_photo_alternate),
+  //               label: Text('Gallery'),
+  //             ),
+  //           ],
+  //         ),
+  //       ],
+  //     ),
+  //   );
+  // }
+
+  Future pickImage(ImageSource source) async {
+    try {
+      print(this.image);
+      final image = await ImagePicker().pickImage(source: source);
+      if (image == null) return;
+      print(image);
+
+      final tempPath = File(image.path);
+
+      print(tempPath);
+      print(tempPath.path);
+
+      setState(() => this.image = tempPath);
+    } on PlatformException catch (e) {
+      print('Failed to pick image: $e');
+    }
   }
 }
